4c0c2c995e3ae1d720f9637a37461aa02f4e1ae1
soheil <soheilkhaledabdi@gmail.com>
2023-11-12 16:43:57 +0330

    Add -> add new route home for home controller

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\AuthController;
+use \App\Http\Controllers\api\HomeController;
 
 
 Route::prefix('v1')->group(function (){
     Route::post('login', [AuthController::class, 'Login']);
     Route::post('register', [AuthController::class, 'Register']);
+});
+
+
+Route::prefix('v1')->middleware('auth:sanctum')->group(function (){
+   Route::get('home' , [HomeController::class , 'index']);
 });
