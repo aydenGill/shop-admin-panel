4c35dd47c458bf9f0211d603fe7f3f4d32ca409e
soheil <soheilkhaledabdi@gmail.com>
2023-11-13 00:07:15 +0330

    Update -> write routes better

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Livewire\Users\All as AllUsers;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::view('/', 'welcome');
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-
-    Route::get("users",\App\Livewire\Users\All::class)->name('admin.users');
+    Route::view('/dashboard','dashboard')->name('dashboard');
+    Route::get('users',AllUsers::class)->name('admin.users');
 });
