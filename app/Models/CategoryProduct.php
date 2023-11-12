602535061b161f8ce2db8451cc3912b62666c692
soheil <soheilkhaledabdi@gmail.com>
2023-11-12 16:42:11 +0330

    Add -> add new model , migration , seeder for category relation

---
+++ app/Models/CategoryProduct.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryProduct extends Model
+{
+    use HasFactory;
+}
