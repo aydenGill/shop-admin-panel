c7b99f566470bf99aa8b3a945bd2d79458743340
soheil <soheilkhaledabdi@gmail.com>
2023-11-12 19:22:17 +0330

    Update -> edit layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,7 +64,9 @@
             <div class="content-wrapper">
                 <!-- Content -->
 
+                <div class="container-xxl flex-grow-1 container-p-y">
                     {{ $slot }}
+                </div>
                 <!-- / Content -->
 
                 <!-- Footer -->
