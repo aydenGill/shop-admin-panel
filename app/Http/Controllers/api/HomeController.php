ff351057644c9e3e6f0affeaba4e26f23de5aca8
soheil <soheilkhaledabdi@gmail.com>
2023-11-12 16:43:20 +0330

    Add -> add home controller for api

---
+++ app/Http/Controllers/api/HomeController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Banner;
+use App\Models\Category;
+use App\Models\LikeProducts;
+use App\Models\Product;
+use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
+use MongoDB\Driver\Exception\EncryptionException;
+
+class HomeController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        try {
+            $banners = Banner::query()->select('id','banner')->get();
+
+            $products = Product::query()->select('id','title','description','price')->get();
+
+            foreach ($products as $product) {
+                $product->likes = $this->calculateLikesForProduct($product->id);
+                $product->isLike = $this->isProductLiked($product->id);
+                $product->rate = 3.5;
+            }
+
+
+            $categories = Category::query()->select('id','name','parent')->get();
+
+            return response()->json(['result' => [
+                'banners' => $banners,
+                'categories' => $categories,
+                'flash_sale' => $products,
+                'newest_product' => [
+                    'expired_at' => Carbon::now()->addDays(5),
+                    'products' => $products
+                ],
+                'most_sale' => $products
+            ] , 'status' => true,'alert' => null ]);
+
+        }catch (EncryptionException $exception){
+            return response()->json(['result' => $exception->getMessage() , 'status' => false ,'alert' => ['title' => 'Error' , 'message' => 'Error form server']], 500);
+        }
+    }
+
+
+    private function calculateLikesForProduct($productId): int
+    {
+        return LikeProducts::query()->where('product_id', $productId)->count();
+    }
+
+    private function isProductLiked($productId): bool
+    {
+        return LikeProducts::query()->where('product_id', $productId)
+            ->where('user_id', auth()->user()->id)
+            ->exists();
+    }
+
+}
