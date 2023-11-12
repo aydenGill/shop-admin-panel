90faa2dd8d269546052b1c80f342be3ce309beec
soheil <soheilkhaledabdi@gmail.com>
2023-11-13 00:07:53 +0330

    Update -> write code upload image

--- app/Livewire/Users/Update.php
+++ app/Livewire/Users/Update.php
@@ -6,17 +6,24 @@ use App\Models\User;
 use Livewire\Attributes\On;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Update extends Component
 {
+
+    use WithFileUploads;
 
     public $id;
 
     #[Rule('required')]
     public $name,$email,$mobile;
 
+    #[Rule('mimes:jpeg,png,jpg,gif|max:2048')]
+    public $image;
+
     #[On('edit-user')]
-    public function edit($id){
+    public function edit($id): void
+    {
         $user = User::query()
             ->select('id','name','mobile','email')
             ->find($id);
@@ -27,16 +34,33 @@ class Update extends Component
         $this->mobile = $user->mobile;
     }
 
-    public function update(){
-        $p=User::query()->findOrFail($this->id);
-        $p->update([
+    public function update(): void
+    {
+        $user = User::query()->findOrFail($this->id);
+
+        $data = [
             'name' => $this->name,
-            'mobile' => $this->mobile
-        ]);
+            'mobile' => $this->mobile,
+        ];
+
+        if (!is_null($this->image)) {
+            $imageUrl = $this->storeUploadedFile();
+            $data['profile_photo_path'] = $imageUrl;
+        }
+
+        $user->update($data);
 
         $this->dispatch('refresh-users');
         $this->dispatch('reset-modal');
     }
+
+
+    private function storeUploadedFile(): string
+    {
+        $randomName = uniqid('image_', true);
+        return $this->image->storeAs('uploads', $randomName, 'public');
+    }
+
     public function render()
     {
         return view('livewire.users.update');
