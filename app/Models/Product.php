57119816b4921fcbe2e6088ab60b2b6734ef774a
soheil <soheilkhaledabdi@gmail.com>
2023-11-12 16:40:50 +0330

    Update -> add fillable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'description',
+        'price',
+        'image',
+        'inventory',
+        'view_count'
+    ];
 }
