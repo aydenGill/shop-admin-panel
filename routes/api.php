42c31d4b0fdfd049780387595f7f2113b971626b
soheil <soheilkhaledabdi@gmail.com>
2023-11-14 19:26:12 +0330

    Clean code -> clean code

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\AuthController;
-use \App\Http\Controllers\api\HomeController;
+use App\Http\Controllers\api\HomeController;
 
 
 Route::prefix('v1')->group(function (){
     Route::post('login', [AuthController::class, 'Login']);
     Route::post('register', [AuthController::class, 'Register']);
 });
-
 
 Route::prefix('v1')->middleware('auth:sanctum')->group(function (){
    Route::get('home' , [HomeController::class , 'index']);
