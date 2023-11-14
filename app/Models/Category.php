26e03c7260ce0ae5d22ebdd6518e1f63e59d2e8d
soheil <soheilkhaledabdi@gmail.com>
2023-11-14 19:20:02 +0330

    Add -> add modal

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -15,4 +16,9 @@ class Category extends Model
         'icon',
         'parent'
     ];
+
+    public function child(): HasMany
+    {
+        return $this->hasMany(Category::class , 'parent' , 'id');
+    }
 }
