a7513d5cf1b167b75b1dd79063d6b5ca59ba3c77
soheil <soheilkhaledabdi@gmail.com>
2023-11-14 19:20:20 +0330

    Update -> add route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use \App\Livewire\Users\All as AllUsers;
+use \App\Livewire\Categories\All as AllCategories;
 
 
 Route::view('/', 'welcome');
@@ -12,4 +13,5 @@ Route::middleware([
 ])->group(function () {
     Route::view('/dashboard','dashboard')->name('dashboard');
     Route::get('users',AllUsers::class)->name('admin.users');
+    Route::get('categories',AllCategories::class)->name('admin.categories');
 });
