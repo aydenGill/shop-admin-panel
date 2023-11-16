b33a5e145ca849f08ad6191b04b8fff4f3563646
soheil <soheilkhaledabdi@gmail.com>
2023-11-17 00:44:04 +0330

    Add -> add new livewire component for update category

---
+++ app/Livewire/Categories/Update.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Livewire\Categories;
+
+use App\Models\Category;
+use Livewire\Attributes\On;
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class Update extends Component
+{
+    use WithFileUploads;
+
+    public $id;
+
+    #[Rule('required|max:255')]
+    public $name ;
+
+    public $parent;
+
+    #[Rule('mimes:jpeg,png,jpg,gif|max:2048')]
+    public $icon;
+
+    public function render()
+    {
+        return view('livewire.categories.update');
+    }
+
+    #[On('update-categories')]
+    public function GetData($id): void
+    {
+        $this->reset(['name', 'parent', 'icon']);
+        $category = Category::query()->findOrFail($id);
+
+        $this->id = $id;
+        $this->name = $category->name;
+        $this->parent = $category->parent;
+    }
+
+    public function update(): void
+    {
+        $this->validate();
+
+        Category::query()->findOrFail($this->id)->update([
+                'name' => $this->name,
+                'parent' => $this->parent ?? 0,
+                'icon' => $this->icon ? $this->storeUploadedFile($this->icon, 'upload') : '',
+        ]);
+
+        $this->dispatch('reset-modal');
+        $this->dispatch('refresh-categories');
+    }
+
+    private function storeUploadedFile($file, $directory): string
+    {
+        $extension = $file->getClientOriginalExtension();
+        $randomName = uniqid('image_', true) . '.' . $extension;
+        return $file->storeAs($directory, $randomName, 'public');
+    }
+
+}
