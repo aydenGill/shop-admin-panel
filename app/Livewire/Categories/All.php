68cf3a86ba9c8b9fe35f5a381affd8a648fca1da
soheil <soheilkhaledabdi@gmail.com>
2023-11-17 00:46:09 +0330

    Update -> write code cleaner

--- app/Livewire/Categories/All.php
+++ app/Livewire/Categories/All.php
@@ -3,93 +3,15 @@
 namespace App\Livewire\Categories;
 
 use App\Models\Category;
-use App\Models\User;
 use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
 class All extends Component
 {
-
     use WithFileUploads;
 
     public $categories;
-
-    public $name , $parent , $icon;
-
-    public $upd_id,$upd_name,$upd_parent_id,$sub_id,$sub_name,$sub_icon,$upd_icon;
-
-    public function mount(){
-        $this->categories = Category::query()->select('id','name','icon')->with('child')->get();
-    }
-
-    public function OpenAddCategoryModal(): void
-    {
-        $this->name = '';
-        $this->parent = 0;
-        $this->icon = '';
-
-        $this->dispatch('OpenAddModal');
-    }
-
-    public function deleteConfirm($id){
-        $category = Category::query()->findOrFail($id);
-        $category->delete();
-        $this->dispatch('refresh-categories');
-    }
-
-    public function save(): void
-    {
-        $data = [
-            'name' => $this->name,
-            'parent' => $this->parent ?? 0
-        ];
-
-        if (!is_null($this->icon)) {
-            $imageUrl = $this->storeUploadedFile();
-            $data['icon'] = $imageUrl;
-        }
-
-        Category::query()->create($data);
-
-        $this->dispatch('reset-modal');
-        $this->dispatch('refresh-categories');
-    }
-
-    public function openEditCategoryModal($id)
-    {
-        $categories = Category::query()->findOrFail($id);
-        $this->upd_name = $categories->name;
-        $this->upd_parent_id = $categories->parent;
-        $this->upd_id = $id;
-
-        $this->dispatch('refresh-categories');
-        $this->dispatch('OpenEditModal');
-    }
-
-    public function update(): void
-    {
-        $this->validate([
-            'upd_name' => 'required|min:3|max:255',
-        ]);
-
-        $data = [
-            'name' => $this->upd_name,
-            'parent' => $this->upd_parent_id ?? 0
-        ];
-
-        if (!is_null($this->upd_icon)) {
-            $imageUrl = $this->storeUploadedUpdateFile();
-            $data['icon'] = $imageUrl;
-        }
-
-        Category::query()->findOrFail($this->upd_id)->update($data);
-
-        $this->dispatch('reset-modal');
-        $this->dispatch('refresh-categories');
-    }
-
-
     public function render()
     {
         return view('livewire.categories.all');
@@ -98,44 +20,24 @@ class All extends Component
     #[On('refresh-categories')]
     public function refreshCategories(): void
     {
-        $this->categories = Category::query()->select('id','name','icon')->with('child')->get();
+        $this->loadCategories();
     }
 
-    public function openSubCategoryModal($id)
+    public function mount(): void
     {
-        $this->sub_name = '';
-        $this->sub_id = $id;
-        $this->dispatch('OpenSubModal');
+        $this->loadCategories();
     }
 
-    public function saveSub()
+    private function loadCategories(): void
     {
-        Category::query()->create([
-            'name' => $this->sub_name,
-            'parent' => $this->sub_id,
-            'icon' => $this->sub_icon
-        ]);
+        $this->categories = Category::all();
     }
 
-    private function storeUploadedFile(): string
+    #[On('goOn-Delete')]
+    function delete($id): void
     {
-        $extension = $this->icon->getClientOriginalExtension();
-        $randomName = uniqid('image_', true) . '.' . $extension;
-        return $this->icon->storeAs('uploads', $randomName, 'public');
+        Category::query()->find($id)->delete();
+        $this->dispatch('deleted');
     }
 
-    private function storeUploadedUpdateFile(): string
-    {
-        $extension = $this->upd_icon->getClientOriginalExtension();
-        $randomName = uniqid('image_', true) . '.' . $extension;
-        return $this->upd_icon->storeAs('uploads', $randomName, 'public');
-    }
-
-
-    private function storeUploadedSubFile(): string
-    {
-        $extension = $this->sub_icon->getClientOriginalExtension();
-        $randomName = uniqid('image_', true) . '.' . $extension;
-        return $this->sub_icon->storeAs('uploads', $randomName, 'public');
-    }
 }
--- resources/views/livewire/categories/all.blade.php
+++ resources/views/livewire/categories/all.blade.php
@@ -1,5 +1,4 @@
-<div>
-    <div class="row">
+ <div class="row">
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
@@ -10,20 +9,56 @@
                             </div>
                             <div class="text-right">
                                 <div class="d-flex">
-                                    <button class="btn btn-primary" wire:click="OpenAddCategoryModal">Add</button>
+                                    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addCategoriesModal"  @click="$dispatch('add-categories')">Add</button>
                                 </div>
                             </div>
                         </div>
                     </div>
-
                     <div class="card-body table-responsive p-0">
-                        @include('livewire.categories.categories-group' , ['categories' => $categories])
+                        @include('layouts.categories-group')
                     </div>
                 </div>
             </div>
         </div>
 
-        @include('livewire.categories.modal.add')
-        @include('livewire.categories.modal.update')
-        @include('livewire.categories.modal.sub')
+     <livewire:categories.add/>
+     <livewire:categories.update/>
+     <livewire:categories.subcategories/>
 </div>
+ <script>
+     document.addEventListener('livewire:initialized',()=>{
+     @this.on('delete-category',(event)=>{
+         swal.fire({
+             title:'Are you sure?',
+             text:'You are about to delete this record, this action is irreversible',
+             icon:'warning',
+             showCancelButton:true,
+             confirmButtonColor:'#3085d6',
+             showCancelButtonColor:'#d33',
+             confirmButtonText:'Yes, Delete it!',
+         }).then((result)=>{
+             if(result.isConfirmed){
+             @this.dispatch('goOn-Delete' , {
+                 id : event.id
+             })
+             @this.on('deleted',(event)=>{
+                 Swal.fire({
+                     position: "top-end",
+                     icon: "success",
+                     title: "Deleted , Your record has been deleted",
+                     showConfirmButton: false,
+                     timer: 1000
+                 });
+             })
+             @this.dispatch('refresh-categories');
+             }
+         })
+     })
+
+         document.addEventListener('livewire:initialized',()=>{
+         @this.on('reset-modal',(event)=>{
+             $('#modalCenter').modal('hide');
+         })
+         })
+     })
+ </script>
