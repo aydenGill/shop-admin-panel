92149cd33d17fb9852a5ddb5a52933a515602f02
soheil <soheilkhaledabdi@gmail.com>
2023-11-17 16:38:43 +0330

    Add -> add helper functions

---
+++ app/Helpers/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+
+function storeUploadedFile($file, $directory): string{
+    $extension = $file->getClientOriginalExtension();
+    $randomName = uniqid('image_', true) . '.' . $extension;
+    return $file->storeAs($directory, $randomName, 'public');
+}
