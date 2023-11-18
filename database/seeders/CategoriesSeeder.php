ea99bf4ec13103aeda6b9cd953b3709ae7ba200e
soheil <soheilkhaledabdi@gmail.com>
2023-11-18 20:53:02 +0330

    Update -> edit seeder

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -15,11 +15,23 @@ class CategoriesSeeder extends Seeder
         Category::query()->truncate();
 
         $categories = [
-            ['name' => 'Clothes' , 'icon' => '' , 'parent' => 0],
-            ['name' => 'Computer' , 'icon' => '','parent' => 0],
-            ['name' => 'Shoes' , 'icon' => '', 'parent' => 0],
-           [ 'name' => 'Watch' , 'icon' => '' , 'parent' => 0],
-           [ 'name' => 'Mobile' , 'icon' => '','parent' => 0]
+            ['name' => 'Computer', 'icon' => '', 'parent' => 0],
+            ['name' => 'Electronics', 'icon' => '', 'parent' => 0],
+            ['name' => 'Arts & Crafts', 'icon' => '', 'parent' => 0],
+            ['name' => 'Automotive', 'icon' => '', 'parent' => 0],
+            ['name' => 'Baby', 'icon' => '', 'parent' => 0],
+            ['name' => 'Beauty and personal care', 'icon' => '', 'parent' => 0],
+            ['name' => 'Women\'s Fashion', 'icon' => '', 'parent' => 0],
+            ['name' => 'Men\'s Fashion', 'icon' => '', 'parent' => 0],
+            ['name' => 'Health and Household', 'icon' => '', 'parent' => 0],
+            ['name' => 'Home and Kitchen', 'icon' => '', 'parent' => 0],
+            ['name' => 'Industrial and Scientific', 'icon' => '', 'parent' => 0],
+            ['name' => 'Luggage', 'icon' => '', 'parent' => 0],
+            ['name' => 'Movies & Television', 'icon' => '', 'parent' => 0],
+            ['name' => 'Pet supplies', 'icon' => '', 'parent' => 0],
+            ['name' => 'Sports and Outdoors', 'icon' => '', 'parent' => 0],
+            ['name' => 'Tools & Home Improvement', 'icon' => '', 'parent' => 0],
+            ['name' => 'Toys and Games', 'icon' => '', 'parent' => 0],
         ];
 
         foreach ($categories as $category) {
