2457b4874acda023fa01e5885a10250d348bf5d4
soheil <soheilkhaledabdi@gmail.com>
2023-11-24 18:32:25 +0330

    Update -> add fillable

--- app/Models/CategoryProduct.php
+++ app/Models/CategoryProduct.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class CategoryProduct extends Model
 {
-    use HasFactory;
+        use HasFactory;
+
+        protected $fillable = [
+            'category_id',
+            'product_id'
+        ];
 }
