d4506430ce05a80a5d50bb9684d1f19d9adaf3bc
soheil <soheilkhaledabdi@gmail.com>
2023-12-08 16:10:40 +0330

    Add -> write a trait for response

---
+++ app/Traits/BaseApiResponse.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+
+trait BaseApiResponse
+{
+    public function success($data, ?string $title = null, ?string $message = null, int $code = 200): JsonResponse
+    {
+        $alert = ($title !== null || $message !== null) ? ['title' => $title, 'message' => $message] : null;
+
+        return response()->json([
+            'result' => $data,
+            'status' => true,
+            'alert' => $alert,
+        ], $code);
+    }
+
+    public function failed($data,string $title = null,string $message = null, int $code = 500): JsonResponse
+    {
+        $alert = ($title !== null || $message !== null) ? ['title' => $title, 'message' => $message] : null;
+
+        return response()->json([
+            'result' => null,
+            'status' => false,
+            'alert' => $alert,
+        ], $code);
+    }
+}
