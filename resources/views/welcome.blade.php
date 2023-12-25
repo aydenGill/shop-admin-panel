bc03270d74a262676cbfdb2cd34d59618d9340cc
soheil khaledabadi <soheilkhaledabdi@gmail.com>
2023-12-25 12:35:35 +0330

    Update -> add my resume to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,132 +4,231 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Soheil Khaledabadi - GitHub Profile</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>soheil khaledabadi - Developer</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Raleway:wght@100&family=Roboto+Mono:wght@100&display=swap"
+        rel="stylesheet">
     <style>
         body {
-            font-family: Arial, sans-serif;
-            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+            color: #333;
+        }
+
+        h1 {
+            font-family: 'Open Sans', sans-serif;
+            font-family: 'Raleway', sans-serif;
+            font-family: 'Roboto Mono', monospace;
         }
 
         header {
-            background-color: #00f1ff;
-            color: white;
-            padding: 40px 20px;
-            text-align: center;
+            padding: 1em 0;
+            box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
         }
 
-        section {
-            padding: 40px 20px;
-            margin: 20px;
-            background-color: white;
-            border-radius: 8px;
-            box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+        nav {
+            margin-left: 20px;
         }
 
-        .projects {
-            display: flex;
-            flex-wrap: wrap;
-            gap: 20px;
-            justify-content: center;
+        nav ul {
+            list-style: none;
+            padding: 0;
         }
 
-        .project-card {
-            background-color: #fff;
-            border-radius: 8px;
-            box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
-            overflow: hidden;
-            width: 300px;
-            max-width: 100%;
-            transition: transform 0.3s ease-in-out;
+        nav ul li {
+            display: inline;
+            margin-right: 20px;
         }
 
-        .project-card:hover {
-            transform: translateY(-5px);
+        nav ul li a {
+            text-decoration: none;
+            color: #333;
         }
 
-        .project-card img {
-            width: 100%;
-            height: 200px;
-            object-fit: cover;
-        }
-
-        .project-card .project-info {
+        .content-section {
+            max-width: 800px;
+            margin: 0 auto;
             padding: 20px;
+            color: #333;
         }
 
-        .project-card h3 {
+        h1 {
+            font-size: 36px;
             margin-top: 0;
+            color: #333;
+        }
+
+        h2 {
+            font-size: 24px;
+            color: #333;
+        }
+
+        p {
+            font-size: 18px;
+            line-height: 1.6;
+            color: #555;
+        }
+
+        ul {
+            list-style-type: disc;
+            padding-left: 20px;
+        }
+
+        .articles-page {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            color: #333;
+        }
+
+        .article {
+            margin-bottom: 20px;
+        }
+
+        #links {
+            padding: 20px;
+            margin-top: 20px;
+        }
+
+        #links h1 {
+            font-size: 24px;
+            color: #333;
+        }
+
+        #links ul {
+            list-style: none;
+            padding: 0;
+        }
+
+        #links li {
+            margin-bottom: 10px;
+        }
+
+        #links a {
+            text-decoration: none;
+            color: #0073e6;
+            /* Link color */
+            font-weight: bold;
+        }
+
+        #links a:hover {
+            color: #0055a5;
+        }
+
+        /* Hover color */
+
+        footer {
+            padding: 10px 0;
+            text-align: center;
+            color: #555;
+        }
+
+
+        @media screen and (max-width: 600px) {
+            h1 {
+                font-size: 28px;
+            }
+
+            h2 {
+                font-size: 20px;
+            }
+
+            p {
+                font-size: 16px;
+            }
         }
     </style>
+
 </head>
 
 <body>
     <header>
-        <h1>Welcome to my GitHub profile!</h1>
-        <p>I'm Soheil Khaledabadi, a passionate backend developer proficient in creating efficient and scalable solutions for web applications. With a focus on clean code and best practices, I strive to deliver high-quality software that meets the needs of end-users 🍀</p>
-        <a href="https://github.com/soheilkhaledabdi" class="btn btn-light">Click here to my GitHub page</a>
+        <nav>
+            <ul>
+                <li><a href="#about">About Me</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#links">Links</a></li>
+                <!-- <li><a href="/articles">Articles</a></li> -->
+            </ul>
+        </nav>
     </header>
 
-    <section class="container my-5">
-        <h2>Socials:</h2>
-        <div class="row">
-            <div class="col-md-4">
-                <a href="https://instagram.com/soheil_khaledabadi" class="btn btn-primary mb-3">Instagram</a>
-            </div>
-            <div class="col-md-4">
-                <a href="https://linkedin.com/in/soheil-khaledabadi-17821621b" class="btn btn-primary mb-3">LinkedIn</a>
-            </div>
-            <div class="col-md-4">
-                <a href="https://stackoverflow.com/users/18247370" class="btn btn-primary mb-3">Stack Overflow</a>
-            </div>
+    <section id="about" class="content-section">
+        <h1>About Me 👨‍💻</h1>
+        <p>
+            Welcome to my personal website. I'm Soheil Khaledabadi, a passionate backend developer dedicated to crafting
+            innovative software solutions. My objective is to transform ideas into reality and continually enhance my
+            skills.
+        </p>
+    </section>
+
+    <section id="skills" class="content-section">
+        <h1>Skills</h1>
+        <p>I'm a backend developer with expertise in the following areas:</p>
+        <ul>
+            <li>Backend Development (PHP, Laravel, GoLang , GIN)</li>
+            <li>Database Management (SQL, Postgres, MySQL)</li>
+            <li>Version Control (Git)</li>
+            <li>Server Management</li>
+            <li>Rest API</li>
+        </ul>
+    </section>
+
+
+
+
+    <section id="experience" class="content-section">
+        <h1>Experience</h1>
+        <div class="experience-item">
+            <h2>Backend Developer at Aserz</h2>
+            <p>
+                I've been with asrez for 1 year and continue to focus on back-end development, working on various
+                exciting projects.
+            </p>
+        </div>
+
+        <div class="experience-item">
+            <h2>Backend Developer at Kashan Tejarat</h2>
+            <p>
+                During my time at Kashan Tejarat, I served as a Backend Developer, contributing to various projects. You
+                can check out some of my work <a href="https://kashantejarat.ir">here</a>.
+            </p>
         </div>
     </section>
 
-    <section class="container my-5">
-        <h2>Tech Stack:</h2>
-        <div class="row">
-            <div class="col-md-2">
-                <img src="https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white" alt="Go" class="img-fluid">
-            </div>
-            <div class="col-md-2">
-                <img src="https://img.shields.io/badge/php-%23777BB4.svg?style=for-the-badge&logo=php&logoColor=white" alt="PHP" class="img-fluid">
-            </div>
-            <div class="col-md-2">
-                <img src="https://img.shields.io/badge/laravel-%23FF2D20.svg?style=for-the-badge&logo=laravel&logoColor=white" alt="Laravel" class="img-fluid">
-            </div>
-            <div class="col-md-2">
-                <img src="https://img.shields.io/badge/apache-%23D42029.svg?style=for-the-badge&logo=apache&logoColor=white" alt="Apache" class="img-fluid">
-            </div>
-            <div class="col-md-2">
-                <img src="https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white" alt="MySQL" class="img-fluid">
-            </div>
-            <div class="col-md-2">
-                <img src="https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white" alt="PostgreSQL" class="img-fluid">
-            </div>
-        </div>
-        <div class="row mt-3">
-            <div class="col-md-2">
-                <img src="https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&logo=postman&logoColor=white" alt="Postman" class="img-fluid">
-            </div>
+    <section id="projects" class="content-section">
+        <h1>Projects</h1>
+        <div class="project-item">
+            <h2>Shopping Admin Panel with Laravel & Livewire</h2>
+            <p>
+                This project involves creating a robust admin panel to manage an e-commerce platform.
+                It utilizes Laravel for the backend structure and Livewire for dynamic frontend interactions.
+                The panel also offers a comprehensive API for scalability and flexibility.
+            </p>
+            <p>GitHub URL: <a href="https://github.com/soheilkhaledabdi/shop-admin-panel">Shop Admin Panel</a></p>
         </div>
     </section>
 
-    <section class="container my-5">
-        <h2>Pinned Projects on GitHub:</h2>
-        <div class="projects">
-            <div class="project-card">
-                <a href="https://github.com/soheilkhaledabdi/shop-admin-panel">
-                    <div class="project-info">
-                        <h3>Shop admin panel</h3>
-                        <p>Effortlessly manage your online store with our Laravel admin panel seamlessly integrated with Livewire, delivering real-time shopping experiences</p>
-                    </div>
-                </a>
-            </div>
-        </div>
+
+    <section id="links" class="content-section">
+        <h1>Links</h1>
+        <ul>
+            <li><a href="https://www.linkedin.com/">LinkedIn</a></li>
+            <li><a href="https://github.com/soheilkhaledabdi/">GitHub</a></li>
+            <li><a href = "mailto: soheilkhaledabdi@gmail.com">Send Email</a></li>
+        </ul>
     </section>
 
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+    <footer>
+        <p>&copy; 2023 <a href="https://soheilkhaledabadi.ir">soheil khaledabadi</a></p>
+    </footer>
 </body>
 
 </html>
