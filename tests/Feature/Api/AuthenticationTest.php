60a8830dd07881e1c29ec4f85db5cb7a4f414a70
soheil khaledabadi <soheilkhaledabdi@gmail.com>
2024-01-07 17:04:38 +0330

    Add -> write test for auth register

---
+++ tests/Feature/Api/AuthenticationTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+    }
+
+    public function test_register_new_user(){
+        $response = $this->post(route('api.register'),[
+            'name' => fake()->name,
+            'email' => fake()->email,
+            'password' => 'password'
+        ]);
+
+        $response->assertStatus(201);
+        $response->assertJson([
+            'status' => true,
+            'alert' => [
+                'title' => 'Registration',
+                'message' => 'Registration successful'
+            ]
+        ]);
+    }
+}
