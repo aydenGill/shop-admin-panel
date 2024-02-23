a011fcbf264e762018f883d34e4cf74d06a6fd8e
soheil khaledabadi <soheilkhaledabdi@gmail.com>
2024-02-23 16:24:33 +0330

    Update -> add migrate to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 cd /var/www/html/shop-admin-panel/ && git pull
+cd /var/www/html/shop-admin-panel/ && php artisan migrate
 cd /var/www/html/shop-admin-panel/ && php artisan optimize:clear
 cd /var/www/html/shop-admin-panel/ && ls -al
